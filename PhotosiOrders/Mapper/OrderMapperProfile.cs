1a0c0f5aded72ae5ae6e7bf562414e2c10d8a45e
lorenzoalessi <alessilorenzo02@gmail.com>
2024-08-13 12:08:36 +0200

    Added mapper profile

---
+++ PhotosiOrders/Mapper/OrderMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using PhotosiOrders.Dto;
+using PhotosiOrders.Model;
+
+namespace PhotosiOrders.Mapper;
+
+public class OrderMapperProfile : Profile
+{
+    public OrderMapperProfile()
+    {
+        CreateMap<Order, OrderDto>()
+            .ReverseMap();
+
+        CreateMap<OrderProduct, OrderProductDto>()
+            .ReverseMap();
+    }
+}
